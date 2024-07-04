3cdb0556303ee3af402d2fdb19c410480c3ab560
MakcimkoDG <sentsegraive@gmail.com>
2024-07-04 18:12:52 +0300

    minor upd

--- js/main.js
+++ js/main.js
@@ -51,11 +51,11 @@ function createRandomId (min, max) {
     return currentValue;
   };
 }
-const commentId = createRandomId(1, PHOTO_NUMBER * MAX_COMMENT_NUMBER);
+const commentId = createRandomId(1, PHOTO_NUMBER * MAX_COMMENT_NUMBER + 1);
 /* генерируем массив со случайным числом комментов */
 function createComment() {
   const comments = [];
-  for(let i = 0; i <= getRandomInt(0,MAX_COMMENT_NUMBER);i++) {
+  for(let i = 0; i <= getRandomInt(0,MAX_COMMENT_NUMBER + 1);i++) {
     const comment = {};
     comment.id = commentId();
     comment.avatar = `img/avatar-${getRandomInt(1, 7)}.svg`;

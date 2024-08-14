79938f456c1f6d5a1c7c8737ac8839cdecb9de8b
MakcimkoDG <sentsegraive@gmail.com>
2024-08-14 23:31:48 +0300

    гзв

--- js/generatePhotos.js
+++ js/generatePhotos.js
@@ -1,7 +1,6 @@
 import {TEMPLATE} from './data.js';
-import {PHOTOS_LIST} from './data.js';
 const PICTURE_TEMPLATE = TEMPLATE.querySelector('.picture');
-
+const PHOTOS_LIST = document.querySelector('section.pictures');
 function addPhotosList (photos) {
   for (let i = 0; i < photos.length;i++) {
     const newPhotoCopy = PICTURE_TEMPLATE.cloneNode(true);
@@ -14,30 +13,6 @@ function addPhotosList (photos) {
     PHOTOS_LIST.appendChild(newPhotoCopy);
   }
 }
+
+
 export {addPhotosList};
-import {createDescription} from './createDescription.js';
-
-const PICTURE_TEMPLATE = TEMPLATE.querySelector('.picture');
-const PHOTOS_LIST = document.querySelector('section.pictures');
-const photoDB = createDescription();
-function addPhotosList () {
-  for (let i = 0; i < photoDB.length;i++) {
-    const newPhotoCopy = PICTURE_TEMPLATE.cloneNode(true);
-    newPhotoCopy.querySelector('.picture__img').src = photoDB[i].url;
-    newPhotoCopy.querySelector('.picture__img').alt = photoDB[i].description;
-    newPhotoCopy.querySelector('.picture__likes').textContent = photoDB[i].likes;
-    newPhotoCopy.querySelector('.picture__comments').textContent = photoDB[i].comments.length;
-    PHOTOS_LIST.appendChild(newPhotoCopy);
-  }
-}
-addPhotosList();
-
-{/* <template id="picture">
-    <a href="#" class="picture">
-      <img class="picture__img" src="" width="182" height="182" alt="Случайная фотография">
-      <p class="picture__info">
-        <span class="picture__comments"></span>
-        <span class="picture__likes"></span>
-      </p>
-    </a>
-  </template> */}
